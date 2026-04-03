acad685c4d7e7291827e9b6e56bf4b82ca036bae
Roshan polai <roshanpolai3@gmail.com>
2026-04-03 20:21:37 +0530

    02Counter project with React and Vite

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(0)
+
+  // method
+  const addValue = () =>{
+    setCounter(counter + 1)
+  }
+
+  const reduceValue = () => {
+    if(counter > 0){
+      setCounter(counter-1)
+    }
+  }
+
+  return (
+    <>
+    <h1>Hello World!</h1>
+    <h2>Counter: {counter}</h2>
+
+    <button onClick={addValue} >Increment</button>
+    <br />
+    <button onClick={reduceValue}>Decrement</button>
+
+    </>
+  )
+}
+
+export default App
